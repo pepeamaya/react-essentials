d962259ba70e8418a72a2e9d3f4bc15d3d20144b
pepeamaya <jose_amaya@epam.com>
2021-07-21 12:00:04 -0500

    useState with reducer function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
+  const [checked, setChecked] = useState(false);
 
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
+  function toggle() { // reducer
+    setChecked((checked) => !checked);
+  }
 
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
 
   return (
     <>
-      <h1> Current emotion is {emotion} and {secondary} </h1>
-      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-      <button onClick={() => setSecondary("crabby")}>Make crabby</button>
+      <input 
+        type="checkbox"
+        value={checked} 
+        onChange={toggle} 
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
