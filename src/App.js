6b614766aeb6bc4338a4e6cfe66b5d00099c52cd
pepeamaya <jose_amaya@epam.com>
2021-07-21 11:32:41 -0500

    useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
 import './App.css';
-// import restaurant from "./restaurant.png";
+import {useState, useEffect} from "react";
 
-function Header(props) {
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
+
   return (
-    <header>
-      <h1>{props.name}'s kitchen</h1>
-    </header>
+    <>
+      <h1> Current emotion is {emotion} and {secondary} </h1>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+      <button onClick={() => setSecondary("crabby")}>Make crabby</button>
+    </>
   );
-}
-
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most delicious food around</p>
-      <img src="https://github.com/josedejesusamaya.png" height={200} alt="Pepe's profile" />
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id} >{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
-}
-
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-      <p>Secret information for authorized users only</p>
-    </footer>
-  );
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Pizza",
-  "Tacos"
-];
-
-const dishObjects = dishes.map((dish, i) => ( {id: i, title: dish} ));
-
-function App(props) {
-  console.log(props);
-  if (props.authorized) {
-    return <Footer year= {new Date().getFullYear()}/>
-  }
-  else {
-    return (
-      <div className="App">
-        <Header name="Pepe" />
-        <Main dishes={dishObjects}/>
-      </div>
-    );
-  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,37 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function AppTwo() {
-  return <h1>This is the second app</h1>;
-}
-
 ReactDOM.render(
-  // React.createElement("h1", { style: {color: "blue"} }, "Hello Pepe"), // first element is what we want to create
-  
-  // React.createElement(
-  //   "ul", 
-  //   null, 
-  //   React.createElement("li", null, "Monday"),
-  //   React.createElement("li", null, "Tuesday"),
-  //   React.createElement("li", null, "Wednesday")
-  //   ),
-  
-  // -> JSX
-  // <ul>
-  //   <li>Monday</li>
-  //   <li>Tuesday</li>
-  //   <li>Wednesday</li>
-  // </ul>,
-  
-  // Fragments
-  // <React.Fragment>
-  //   <App />
-  //   <AppTwo />
-  // </React.Fragment>,
-  <>
-    <App authorized={true}/>
-    <AppTwo />
-  </>,
+  <App />,
   document.getElementById('root') // where we want to put it
 );
 
